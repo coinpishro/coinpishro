ef5804036a553f29cf9d6cdc9781ebd9fa692fae
coinpishro <140241065+coinpishro@users.noreply.github.com>
2023-07-22 12:39:35 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x3a90f79562899d22d5a5a3ebdd5f69d0f3cdb9e6";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
